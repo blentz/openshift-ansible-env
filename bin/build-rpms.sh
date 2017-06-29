52df1bb2f615d06db28b68f44099b7bd5efc079f
Brett Lentz <blentz@redhat.com>
2017-06-29 14:35:42 -0400

    inventory handling, additional requirements checking, plus comments.

--- bin/build-rpms.sh
+++ bin/build-rpms.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RPMNAME="tito"
+if [ "$(rpm -q ${RPMNAME})" == "package ${RPMNAME} is not installed" ]; then
+    echo "**** ${RPMNAME} is required to build RPMs. Please install it. ****"
+    exit 1
+fi
+
 [ ! -d rpms ] && mkdir rpms
 
 pushd git/openshift-tools
